1f225d1bf48abfa356f78c9913ab143e9b6c98d4
Eduardo Ramos (edu159) <eduradical951@gmail.com>
2014-07-03 11:31:48 +0200

    Added benchmark/check_sums.m to the repo.

---
+++ benchmark/check_sums.m
@@ -0,0 +1,13 @@
+b = L * U;
+dim = 1;
+if (setup.milu == 'row')
+  dim = 2;
+endif
+c = sum (b, dim);
+d = sum (A, dim);
+v = abs (c - d);
+num_zeros = length (find (v > sqrt (eps)));
+printf('Number of rows-columns not well compensated: %d\n', num_zeros);
+if (num_zeros > 0)
+  v (find (v > sqrt (eps)))
+endif
